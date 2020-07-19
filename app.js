262fb07df04669093f0dc304e0d9b98db05ce231
Gal Ben yosef <galbenyosef@users.noreply.github.com>
2020-07-19 18:46:15 +0300

    test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const indexRouter = require('./routes/index');
 
@@ -15,7 +16,13 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(createProxyMiddleware({
+  target: 'https://tlt.kala-crm.co.il',
+  changeOrigin: true,
+  pathRewrite: {
+    '^/kala':'/api'
+  }
+}))
 app.use('/', indexRouter);
 
 
